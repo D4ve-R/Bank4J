cc5571801e6856980509ecdfff1e28a4facdbd19
David R <david.rechkemmer@googlemail.com>
2021-10-22 13:29:40 +0200

    javadoc & CalculateBill class

--- bank/Transaction.java
+++ bank/Transaction.java
@@ -1,38 +1,77 @@
 package bank;
 
-//SuperClass
-public class Transaction{
+/**
+ * Transaction Class, super class fro Transfer & Payment
+ * implements CalculateBill class , @see CalculateBill
+ */
+public class Transaction implements CalculateBill{
     private String date;
     private double amount;
     private String description;
 
-    //Constructor
+    /**
+     * Constructor method, sets attributes
+     * @param date Date of Transaction
+     * @param amount Amount of Transaction
+     * @param description Description
+     */
     protected Transaction(String date, double amount, String description){
         this.date = date;
         this.amount = amount;
         this.description = description;
     }
 
-    /*
-    setter & getter methods
+    /**
+     * set Date of Transaction
+     * @param date
      */
     protected void setDate(String date){
         this.date = date;
     }
+
+    /**
+     * set Amount of Transaction
+     * @param amount
+     */
     protected void setAmount(double amount){
         this.amount = amount;
     }
+
+    /**
+     * set Descritption of Transaction
+     * @param description
+     */
     protected void setDescription(String description){
         this.description = description;
     }
+
+    /**
+     * get Date of Transaction
+     * @return date
+     */
     protected String getDate(){
         return date;
     }
+
+    /**
+     * get Amount of Transaction
+     * @return amount
+     */
     protected double getAmount(){
         return amount;
     }
-    protected String getDescription() { return description; }
 
+    /**
+     * get Description of Transaction
+     * @return description
+     */
+    protected String getDescription(){
+        return description;
+    }
+
+    /**
+     * prints Transaction Data
+     */
     protected void printTransactionData(){
         System.out.println("Date: " + date);
         System.out.println("Amount: " + amount);
--- bank/Transfer.java
+++ bank/Transfer.java
@@ -2,54 +2,101 @@ package bank;               //defines package-env of the following code
 import bank.Transaction;    //import class from package
 
 
-//Transfer Class derived from Transaction Class
+/**
+ * Transfer Class inherits from super Class Transaction
+ * @see Transaction
+ */
 public class Transfer extends Transaction{
     private String sender;
     private String recipient;
 
-    //Super-Constructor
+    /**
+     * Constructor method, call Constructor of Transaction
+     * @param date Date of Transfer
+     * @param amount Amount of Transfer
+     * @param descrp Description
+     */
     public Transfer(String date, double amount, String descrp){
         super(date, amount, descrp);
     }
 
-    //Constructor
+    /**
+     * Constructor for Transfer, calls super constructor,
+     * sets sender, recipient
+     * @param date Date of Transfer
+     * @param amount Amoutn of Transfer
+     * @param descrp Description
+     * @param sender Sender of Transfer
+     * @param recipient Recipient of Transfer
+     */
     public Transfer(String date, double amount, String descrp, String sender, String recipient){
         super(date, amount, descrp);
         this.sender = sender;
         this.recipient = recipient;
     }
 
-    //Copy-Constructor
+    /**
+     * Copy Constructor for Transfer
+     * @param trans Transfer Object
+     */
     public Transfer(Transfer trans){
         super(trans.getDate(), trans.getAmount(), trans.getDescription());
         sender = trans.getSender();
         recipient = trans.getRecipient();
     }
 
-    /*
-    setter methods
+    /**
+     * set Sender of Transfer
+     * @param sender Sender of Transfer
      */
     public void setSender(String sender){
         this.sender = sender;
     }
+
+    /**
+     * set Recipient
+     * @param recipient
+     */
     public void setRecipient(String recipient){
         this.recipient = recipient;
     }
 
-    /*
-    getter methods
+    /**
+     * get Sender
+     * @return sender
      */
-    public String getSender() { return sender; }
+    public String getSender(){
+        return sender;
+    }
+
+    /**
+     * getRecipient
+     * @return recipient
+     */
     public String getRecipient(){
         return recipient;
     }
 
-    //print out Object
+    /**
+     * prints out Object
+     */
     public void printObj(){
         System.out.println("{");
         printTransactionData();
         System.out.println("Sender: " + sender);
         System.out.println("Recipient: " + recipient);
         System.out.println("}");
+    }
+
+
+    /**
+     * calculate interest
+     * overwritten from CalculateBill Class
+     * @see CalculateBill
+     * @return amount
+     */
+    @Override
+    public double calculate(){
+        return amount;
     }
 }
